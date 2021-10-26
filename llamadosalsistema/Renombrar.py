93f1fe9b9983c25079bf9d5216e4d0ee497ef642
jguavitah <92111740+jguavitah@users.noreply.github.com>
2021-10-26 09:31:27 -0500

    Delete llamadosalsistema directory

--- llamadosalsistema/Renombrar.py
+++
@@ -1,8 +0,0 @@
-import os
-print("nombre del archivo a cambiar el nombre")
-NAME = input()
-print("nuevo nombre:")
-NEW_NAME= input()
-def Rename(a,b):
-    os.rename(a,b) # os.rename cumple la funcion de cambiar el nombre de un archivo
-Rename(NAME,NEW_NAME)
--- llamadosalsistema/crearcarpeta.py
+++
@@ -1,7 +0,0 @@
-import os
-print("Nombre de la carpeta a crear:")
-FOLDER_NAME = input() 
-def Make_folder(a) :
-    os.mkdir(a) # os.mkdir permite crear un directorio nuevo 
-
-Make_folder(FOLDER_NAME)
