46d63992588a3e081e31b854718b2be90015a0ec
jguavitah <92111740+jguavitah@users.noreply.github.com>
2021-11-06 18:47:09 -0500

    Update punto1.py

--- parcial 1/punto1.py
+++ parcial 1/punto1.py
@@ -4,12 +4,12 @@ import os
 p = os.path.dirname(os.getcwd()) #devuelve el path del directorio donde se encuentra actualmente el worksapce
 os.chdir(p+ "/parcial 1")
 def escribir():
-    A = open('funcioneOS.txt','a')
+    A = open('funcioneOS.txt','w')
     A.write("""Funcion de un sistema operativo
-    1. multitareas
+    1. Gestionar procesos o recursos para que los programas puedan ejecutarse de manera correcta.
     2.multiusuario capacidad de identificar diferentes usuarios y asignar privilegios
-    3.multiprocesamiento o multiprocesador
-    4capacidad de dadministrar maquinas con varios cpus
+    3.Administrar la memoria principal del dispositivo, de modo que aunque varios programas se pongan en marcha, cada uno cuente con una entrada de memoria independiente
+    4.Detectar errores, mantener la operatividad y controlar dispositivos, de manera que se eviten las interrupciones.
     5.multiprocesamiento asimetrico y simetrico""")
     A.close()
 
